0c1401167ce3f584a182e46acb9159f9f487e2d9
Tsolmon <24HP0016@G305-CC0052.local>
2024-11-05 18:12:32 +0800

    end

--- server.js
+++ server.js
@@ -6,6 +6,28 @@ const PORT = 8080;
 
 const data = require("/Users/24HP0016/Desktop/Back-end/express/mockData.json");
 
+const checkLogin = (req, res, next) => {
+  const reqName = req.body.name;
+  const reqPass = req.body.password;
+  const currentUser = data.find(
+    (user) => user.name === reqName && user.password === reqPass
+  );
+  if (!currentUser) {
+    res.send("Name or Password incorrect");
+  }
+  next();
+};
+
+const signUpUser = (req, res, next) => {
+  const body = req.body;
+  const findingUser = data.find((user) => user.name === body.name);
+  if (findingUser) {
+    res.send("Already taken");
+  } else {
+    next();
+  }
+};
+
 app.get("/users", (req, res) => {
   const url = req.url;
   if (url.startsWith(`/users?`)) {
@@ -16,34 +38,29 @@ app.get("/users", (req, res) => {
     res.send(data);
   }
 });
-app.post("/login", (req, res) => {
-  const reqName = req.body.name;
-  const reqPass = req.body.password;
-  const currentUser = data.find(
-    (user) => user.name === reqName && user.password === reqPass
-  );
-  if (currentUser) {
-    res.send(currentUser);
-  } else {
-    res.send("not found");
-  }
+app.post("/login", checkLogin, (req, res) => {
+  res.send("amjilltai nevterlee");
 });
 
-app.post("/signup", (req, res) => {
+app.post("/signup", signUpUser, (req, res) => {
   const body = req.body;
-  const findingUser = data.find((user) => user.name === body.name);
-  if (findingUser) {
-    res.send("Already taken");
-  } else {
-    const newUser = {
-      id: JSON.stringify(data.length + 1),
-      ...body,
-    };
-    data.push(newUser);
-    fs.writeFileSync("mockData.json", JSON.stringify(data), (err) => {
-      console.log(err);
-    });
-    res.send("done");
-  }
+  const newUser = {
+    id: JSON.stringify(data.length + 1),
+    ...body,
+  };
+  data.push(newUser);
+  fs.writeFileSync("mockData.json", JSON.stringify(data), (err) => {
+    console.log(err);
+  });
+  res.send("done");
+});
+
+app.delete("/delete", (req, res) => {
+  const body = req.body;
+  const deletingUser = data.filter((user) => user.id !== body.id);
+  fs.writeFileSync("mockData.json", JSON.stringify(deletingUser), (err) => {
+    console.log(err);
+  });
+  res.send("Done");
 });
 app.listen(PORT);
